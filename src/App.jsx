905a87d1f72d9564842e4e89756416424f5498e3
kaiiisaka <panches2001@gmail.com>
2023-02-17 14:45:09 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,11 @@ function App() {
 
   return (
     <div className="App min-h-screen h-full bg-scroll bg-origin-content bg-cover flex flex-col justify-center items-center" style={{backgroundImage: `url(${bg})`}}>
+          <h1 id='text222' className='text222 text-9xl font-bold' style={{fontFamily: 'Special Elite'}}></h1>
+        <h1 className='text222 text-6xl font-bold animate-pulse' style={{fontFamily: 'Special Elite'}}>Wait for reward...</h1>
         <div>
             <img src={banana} alt='banana' className='banana h-16'/>
         </div>
-          <h1 id='text222' className='text222 text-9xl font-bold' style={{fontFamily: 'Special Elite'}}></h1>
     </div>
   )
 }
